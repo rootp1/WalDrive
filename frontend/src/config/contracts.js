a6ccee18e3139af7fdc76470bc3ccc84f0e6bfd5
rootp1 <arnav.iitr@gmail.com>
2025-11-19 16:30:35 +0530

    Install Sui dependencies

---
+++ frontend/src/config/contracts.js
@@ -0,0 +1,16 @@
+export const NETWORK = 'testnet';
+
+export const PACKAGE_ID = '0x3952b085766fe6d11af5e988e04886ff0627ef7d2378a88dd783ef58d15cae7d';
+
+export const MODULE_NAMES = {
+  FILE_METADATA: 'file_metadata',
+  FOLDER_REGISTRY: 'folder_registry',
+  USER_PROFILE: 'user_profile',
+  SHARING: 'sharing',
+};
+
+export const WALRUS_CONFIG = {
+  AGGREGATOR_URL: 'https://aggregator.walrus-testnet.walrus.space',
+  PUBLISHER_URL: 'https://publisher.walrus-testnet.walrus.space',
+  EPOCHS: 5,
+};
